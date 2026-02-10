66d4c0edbc8b63d855bc35f8f906a4e5c6aa9819
matus1888 <matus1888@gmail.com>
2026-02-10 09:03:44 +0300

    fix: proxy file path

--- lua/scratches/proxy/proxy.sh
+++ lua/scratches/proxy/proxy.sh
@@ -3,4 +3,6 @@
 IP_ADDRESS=$(grep -oP 'nameserver \K[0-9.]+' /etc/resolv.conf)
 
 echo "IP Address of the host system: $IP_ADDRESS"
-node proxy.js 11434 "$IP_ADDRESS" 11434
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+echo script_directory = "$SCRIPT_DIR"
+node "${SCRIPT_DIR}/fromWSLProxy.js" 11434 "$IP_ADDRESS" 11434
